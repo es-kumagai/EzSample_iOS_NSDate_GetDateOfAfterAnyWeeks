62f1408f578e4e2e320dad8ef54ab50bbd9fbf57
Tomohiro Kumagai <tomohiro@ez-style.jp>
2012-08-07 22:30:38 +0900

    Initial Commit

---
+++ EzSample_iOS_NSDate_GetDateOfAfterAnyWeeks/EzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EzAppDelegate.h
+//  EzSample_iOS_NSDate_GetDateOfAfterAnyWeeks
+//
+//  Created by 熊谷 友宏 on H.24/08/07.
+//  Copyright (c) 平成24年 Tomohiro Kumagai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
